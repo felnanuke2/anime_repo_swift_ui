c8e8c9ac7fe9f1caff27e4014fb9e93df7e8bcab
Luiz Felipe <felnanuke@gmail.com>
2021-11-02 13:56:56 -0300

    Initial Commit

---
+++ Anime Repo/Anime_RepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anime_RepoApp.swift
+//  Anime Repo
+//
+//  Created by LUIZ FELIPE ALVES LIMA on 02/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Anime_RepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anime Repo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Anime Repo
+//
+//  Created by LUIZ FELIPE ALVES LIMA on 02/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
